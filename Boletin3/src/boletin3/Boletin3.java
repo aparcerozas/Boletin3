d9130e3b5b380387b5adff4db20cf9144279164d
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2018-10-17 12:23:29 +0200

    Boletin3

---
+++ Boletin3/src/boletin3/Boletin3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin3;
+
+/**
+ *
+ * @author aparcerozas
+ */
+public class Boletin3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Consumo consumo1 = new Consumo();
+        consumo1.setLitros(50f);
+        consumo1.setPgas(1.57f);
+        Consumo consumo2 = new Consumo(451f, 65.7f, 74.3f, 1.57f);
+        System.out.println("consumo medio = " + consumo2.consumoMedio());
+        consumo2.setLitros(56.2f);
+        System.out.println("velocidade media" + consumo2.getVmed());
+    }
+       
+}
---
+++ Boletin3/src/boletin3/Consumo.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin3;
+
+/**
+ *
+ * @author aparcerozas
+ */
+public class Consumo {
+    private float km;
+    private float litros;
+    private float vMed;
+    private float pGas;
+    
+    public Consumo(){}
+    public Consumo(float km, float litros, float vMed, float pGas){
+        this.km = km;
+        this.litros = litros;
+        this.vMed = vMed;
+        this.pGas = pGas;
+    }
+    
+    public float getTempo(){
+        float tempo = km / vMed;
+        return tempo;
+    }
+    public float consumoMedio(){
+        float cMed = litros / km * 100;
+        return cMed;
+    }
+    public float consumoEuros(){
+        float cEuros = pGas / this.consumoMedio() * 100;
+        return cEuros;
+    }
+    public float getKm(){
+        return km;
+    }
+    public float getLitros(){
+        return litros;
+    }
+    public float getVmed(){
+        return vMed;
+    }
+    public float getPgas(){
+        return pGas;
+    }
+    public void setKm(float km){
+        this.km = km;
+    }
+    public void setLitros(float litros){
+        this.litros = litros;
+    }
+    public void setVmed(float vMed){
+        this.vMed = vMed;
+    }
+    public void setPgas(float pGas){
+        this.pGas = pGas;
+    }
+    
+}
